66b63ef20b8f58718a745fa1e237e28414cea544
mc.coquio <mc.coquio@gmail.com>
2022-06-16 00:27:55 +0200

    ajout middleware err et fn async handler

--- server.js
+++ server.js
@@ -4,19 +4,21 @@ const PORT = process.env.PORT || 5000;
 const morgan = require('morgan');
 const colors = require('colors');
 const connectDB = require('./config/db');
+const errorHandler = require('./midddleware/error');
 
 //Chargement des variables d'environement
 dotenv.config({ path: './config/config.env' });
 
 //Connexion a la BDD
 connectDB();
-// console.log(connectDB());
-
 
 //route fichier
 const bootcamps = require('./routes/bootcamps');
 
+
 const app = express();
+
+
 
 // Body Parser
 app.use(express.json());
@@ -30,6 +32,8 @@ if(process.env.NODE_ENV === 'developpement') {
 //chargement des routes 
 app.use('/api/v1/bootcamps', bootcamps);
 
+//errorHandler
+app.use(errorHandler);
 
 const server = app.listen(PORT,
     () => console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold)
